fbe31a05a62381d2bd801ac4ebcc7350801674bb
Uladzimir Klyshevich <istin2007@gmail.com>
2017-10-02 18:51:43 +0300

    intial setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # android-training-2017
 Project for android training
+
+
+#old one
+
+https://github.com/deniotokiari/training-epam-2016
---
+++ app/src/main/java/com/epam/androidtraining/MainActivity.java
@@ -0,0 +1,14 @@
+package com.epam.androidtraining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
