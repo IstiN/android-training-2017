f1155bb0ff6624ef45bd42bb0f784a89b5468098
Uladzimir Klyshevich <istin2007@gmail.com>
2017-10-02 19:04:57 +0300

    added some changes to git

--- app/src/main/java/com/epam/androidtraining/MainActivity.java
+++ app/src/main/java/com/epam/androidtraining/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //changes in new branch
     }
 
 }
