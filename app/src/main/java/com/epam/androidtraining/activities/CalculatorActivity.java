3b7f1a205a8d6969fcbf7b31fd79f1a8394fcaaf
Alex Dzeshko <a.dzeshko@gmail.com>
2017-11-07 16:43:51 +0300

    update gradle files

--- app/src/main/java/com/epam/androidtraining/activities/CalculatorActivity.java
+++ app/src/main/java/com/epam/androidtraining/activities/CalculatorActivity.java
@@ -17,14 +17,44 @@ public class CalculatorActivity extends AppCompatActivity {
         super.onCreate(pSavedInstanceState);
         setContentView(R.layout.calculactor_linear);
 
+        mExpressionView = findViewById(R.id.expression_text);
         View.OnClickListener clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mExpressionView.append(((TextView) v).getText());
             }
         };
+        findViewById(R.id.btn_0).setOnClickListener(clickListener);
         findViewById(R.id.btn_1).setOnClickListener(clickListener);
-        mExpressionView = findViewById(R.id.expression_text);
+        findViewById(R.id.btn_2).setOnClickListener(clickListener);
+        findViewById(R.id.btn_3).setOnClickListener(clickListener);
+        findViewById(R.id.btn_4).setOnClickListener(clickListener);
+        findViewById(R.id.btn_5).setOnClickListener(clickListener);
+        findViewById(R.id.btn_6).setOnClickListener(clickListener);
+        findViewById(R.id.btn_7).setOnClickListener(clickListener);
+        findViewById(R.id.btn_8).setOnClickListener(clickListener);
+        findViewById(R.id.btn_9).setOnClickListener(clickListener);
+        findViewById(R.id.btn_plus).setOnClickListener(clickListener);
+        findViewById(R.id.btn_minus).setOnClickListener(clickListener);
+        findViewById(R.id.btn_multiply).setOnClickListener(clickListener);
+        findViewById(R.id.btn_divide).setOnClickListener(clickListener);
+        findViewById(R.id.btn_dot).setOnClickListener(clickListener);
+
+        findViewById(R.id.btn_clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mExpressionView.setText("");
+            }
+        });
+        findViewById(R.id.btn_del).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence text = mExpressionView.getText();
+                if (text.length() > 0) {
+                    mExpressionView.setText(text.subSequence(0, text.length() - 1));
+                }
+            }
+        });
 
     }
 
